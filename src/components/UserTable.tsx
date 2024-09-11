1d89596cd506bedbd64afb88b87156b2cca2866d
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-09-11 13:35:57 +0200

    Make user table responsive for mobile

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -6,34 +6,36 @@ function UserTable({ users, loading, error, filter }: UsersState) {
   if (error) return <p>{error}</p>;
 
   return (
-    <table className="table-fixed w-full">
-      <thead className="text-xl border-b-2 border-slate-200 bg-slate-800 text-slate-200">
-        <tr className="divide-x-2 divide-slate-200">
-          <th className="text-start p-4">Name</th>
-          <th className="text-start py-2 px-4">Username</th>
-          <th className="text-start py-2 px-4">Email</th>
-          <th className="text-start py-2 px-4">Phone</th>
-        </tr>
-      </thead>
-      <tbody className="text-lg bg-slate-700 divide-y divide-slate-400 text-slate-300">
-        {users.map((user) => (
-          <tr key={user.id} className="divide-x divide-slate-400">
-            <td className="py-2 px-4">
-              <HighlightedText text={user.name} highlight={filter} />
-            </td>
-            <td className="py-2 px-4">
-              <HighlightedText text={user.username} highlight={filter} />
-            </td>
-            <td className="py-2 px-4">
-              <HighlightedText text={user.email} highlight={filter} />
-            </td>
-            <td className="py-2 px-4">
-              <HighlightedText text={user.phone} highlight={filter} />
-            </td>
+    <div className="overflow-x-scroll">
+      <table className="table-fixed lg:w-full text-nowrap">
+        <thead className="md:text-xl border-b-2 border-slate-200 bg-slate-800 text-slate-200">
+          <tr className="divide-x-2 divide-slate-200">
+            <th className="text-start p-4">Name</th>
+            <th className="text-start py-2 px-4">Username</th>
+            <th className="text-start py-2 px-4">Email</th>
+            <th className="text-start py-2 px-4">Phone</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="text-sm md:text-lg bg-slate-700 divide-y divide-slate-400 text-slate-300">
+          {users.map((user) => (
+            <tr key={user.id} className="divide-x divide-slate-400">
+              <td className="py-2 px-4">
+                <HighlightedText text={user.name} highlight={filter} />
+              </td>
+              <td className="py-2 px-4">
+                <HighlightedText text={user.username} highlight={filter} />
+              </td>
+              <td className="py-2 px-4">
+                <HighlightedText text={user.email} highlight={filter} />
+              </td>
+              <td className="py-2 px-4">
+                <HighlightedText text={user.phone} highlight={filter} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
